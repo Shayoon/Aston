d8c9152c038aba007d720c4843081e926e8f33d5
Shayoon <quentin.degorgue@gmail.com>
2022-05-09 13:56:14 +0200

    ajout fonctionnalité calculatrice

--- src/fr/exo6/Calculatrice.java
+++ src/fr/exo6/Calculatrice.java
@@ -4,23 +4,25 @@ import java.util.Scanner;
 
 public class Calculatrice {
     public static void calculatrice() {
-        int nbr1 = demanderSaisieInt("Entrez le premier nombre");
+        float nbr1 = demanderSaisieFloat("Entrez le premier nombre");
         String operateur = demanderSaisieOperateur("Entrez l'operateur (* . / . + . -)");
-        int nbr2 = demanderSaisieInt("Entrez le second nombre");
-
+        float nbr2 = demanderSaisieFloat("Entrez le second nombre");
+        if (operateur.equals("/") && nbr2 == 0)
+            System.out.println("Impossible de diviser par 0");
+        else 
         switch (operateur) {
-            case "*" -> System.out.printf("%d * %d = %d", nbr1, nbr2, nbr1 * nbr2);
-            case "/" -> System.out.printf("%d / %d = %f", nbr1, nbr2, (float)nbr1 / nbr2);
-            case "+" -> System.out.printf("%d + %d = %d", nbr1, nbr2, nbr1 + nbr2);
-            case "-" -> System.out.printf("%d - %d = %d", nbr1, nbr2, nbr1 - nbr2);
+            case "*" -> System.out.printf("%f * %f = %f", nbr1, nbr2, nbr1 * nbr2);
+            case "/" -> System.out.printf("%f / %f = %f", nbr1, nbr2, nbr1 / nbr2);
+            case "+" -> System.out.printf("%f + %f = %d", nbr1, nbr2, nbr1 + nbr2);
+            case "-" -> System.out.printf("%f - %f = %d", nbr1, nbr2, nbr1 - nbr2);
         }
 
     }
 
-    public static int demanderSaisieInt(String message) {
+    public static Float demanderSaisieFloat(String message) {
         Scanner scanner = new Scanner(System.in);
         System.out.println(message);
-        return scanner.nextInt();
+        return scanner.nextFloat();
     }
 
     public static String demanderSaisieOperateur(String message) {
