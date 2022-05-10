f7419272d5035e307f0f4292379bcefc12fe2747
Shayoon <quentin.degorgue@gmail.com>
2022-05-10 15:41:35 +0200

    Version optimisée temperature

--- src/fr/exo12/Temperature.java
+++ src/fr/exo12/Temperature.java
@@ -4,34 +4,15 @@ public class Temperature {
 
     public static void plusProche(int[]temperatures){
         int plusProche = temperatures[0];
-        int distancePlusProche = 0;
-        int distance;
+        for (int temperature : temperatures) {
+            if (temperature < 0) {
+                if (Math.abs(temperature) <= Math.abs(plusProche)) {
+                    plusProche = temperature;
+                }
+            } else if (temperature < Math.abs(plusProche)) {
+                plusProche = temperature;
+            }
 
-        for (int i = 0; i < temperatures.length; i++) {
-            int temperature = temperatures[i];
-            if (i == 0){
-                if(temperature > 0){
-                    plusProche = temperature;
-                    distancePlusProche = temperature;
-                }
-                else {
-                    distancePlusProche = -temperature;
-                    plusProche = temperature;
-                }
-            }
-            if (temperature < 0){
-                distance = -temperature;
-                if (distance <= distancePlusProche){
-                    plusProche = temperature;
-                    distancePlusProche = distance;
-                }
-            }
-            else {
-                if (temperature < distancePlusProche){
-                    plusProche = temperature;
-                    distancePlusProche = temperature;
-                }
-            }
         }
         System.out.println("Le plus proche est : " + plusProche);
     }
